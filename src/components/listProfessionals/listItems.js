82c4a3dc1467500c3ca1802af1590a2dfbba60c4
camilo.bello92@outlook.es <camilo.bello92@gmail.com>
2020-02-11 00:49:18 -0300

    working with react context and hooks

--- src/components/listProfessionals/listItems.js
+++ src/components/listProfessionals/listItems.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Rating from "@material-ui/lab/Rating";
 import Box from "@material-ui/core/Box";
 import ListItem from "@material-ui/core/ListItem";
@@ -9,6 +9,7 @@ import Avatar from "@material-ui/core/Avatar";
 import { List } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import { AppContext } from "../../context/ListProfessionalsContext";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,100 +24,70 @@ const useStyles = makeStyles(theme => ({
 export default function AlignItemsList() {
   const classes = useStyles();
 
+  const { setopenFullScreenModal, setProfessionalSelected } = useContext(AppContext);
+  const data = [
+    {
+      name: "Juan perez",
+      img: "/img/avatars/1.jpg",
+      comune: "Lota - Chile",
+      ranking: 5
+    },
+    {
+      name: "Javier Alvarez",
+      img: "/img/avatars/2.jpg",
+      comune: "Penco - Chile",
+      ranking: 3
+    },
+    {
+      name: "Maria Nuñez",
+      img: "/img/avatars/3.jpg",
+      comune: "Concepción - Chile",
+      ranking: 4
+    }
+  ];
   return (
     <List className={classes.root}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/img/avatars/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
-        />
-        <Box component="fieldset" mb={3} borderColor="transparent">
-          <Rating
-            name="simple-controlled"
-            value={2}
-            // onChange={(event, newValue) => {
-            //   setValue(newValue);
-            // }}
-          />
-        </Box>
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Travis Howard" src="/img/avatars/2.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Summer BBQ"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                to Scott, Alex, Jennifer
-              </Typography>
-              {" — Wish I could come, but I'm out of town this…"}
-            </React.Fragment>
-          }
-        />
-        <Box component="fieldset" mb={3} borderColor="transparent">
-          <Rating
-            name="simple-controlled"
-            value={5}
-            // onChange={(event, newValue) => {
-            //   setValue(newValue);
-            // }}
-          />
-        </Box>
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Cindy Baker" src="/img/avatars/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Oui Oui"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Sandra Adams
-              </Typography>
-              {" — Do you have Paris recommendations? Have you ever…"}
-            </React.Fragment>
-          }
-        />
-        <Box component="fieldset" mb={3} borderColor="transparent">
-          <Rating
-            name="simple-controlled"
-            value={3}
-            // onChange={(event, newValue) => {
-            //   setValue(newValue);
-            // }}
-          />
-        </Box>
-      </ListItem>
+      {data.map((e, i) => (
+        <div key={i}>
+          <ListItem
+            alignItems="flex-start"
+            onClick={() => {
+              setProfessionalSelected(e);
+              setopenFullScreenModal(true);
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src={e.img} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={e.name}
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {e.comune}
+                </React.Fragment>
+              }
+            />
+            <Box component="fieldset" mb={3} borderColor="transparent">
+              <Rating
+                name="simple-controlled"
+                value={e.ranking}
+                // onChange={(event, newValue) => {
+                //   setValue(newValue);
+                // }}
+              />
+            </Box>
+          </ListItem>
+          <Divider variant="inset" component="li" />
+        </div>
+      ))}
     </List>
   );
 }
--- src/views/listProfessionals/index.js
+++ src/views/listProfessionals/index.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
-import ListItems from '../../components/listProfessionals/listItems';
+import ListItems from "../../components/listProfessionals/listItems";
+import { AppContext } from "../../context/ListProfessionalsContext";
+import ViewDisponibility from '../../components/listProfessionals/viewDisponibility';
 
 export default function AlignItemsList() {
+  // definimos los state que deseamos modificar a través del provider
+  const [openFullScreenModal, setopenFullScreenModal] = useState(false);
+  const [professionalSelected, setProfessionalSelected] = useState(undefined);
   return (
-    <React.Fragment>
-      <Grid container spacing={2}>
-        <Grid item lg={6} xs={4}>
-          <Typography component="legend">EDUCACIÓN</Typography>
+    // definimos el provider
+    <AppContext.Provider value={{ 
+      openFullScreenModal, 
+      setopenFullScreenModal, 
+      professionalSelected,
+      setProfessionalSelected 
+      }}>
+      <React.Fragment>
+        <Grid container spacing={2}>
+          <Grid item lg={6} xs={4}>
+            <Typography component="legend">EDUCACIÓN</Typography>
+          </Grid>
+          <Grid item lg={6} xs={12}>
+            <Typography component="legend" style={{ textAlign: "right" }}>
+              {new Date().toDateString()}
+            </Typography>
+          </Grid>
+          <ListItems></ListItems>
+          <ViewDisponibility />
         </Grid>
-        <Grid item lg={6} xs={12}>
-          <Typography component="legend" style={{ textAlign: "right" }}>
-            { new Date().toDateString() }
-          </Typography>
-        </Grid>
-        <ListItems></ListItems>
-      </Grid>
-    </React.Fragment>
+      </React.Fragment>
+    </AppContext.Provider>
   );
 }
