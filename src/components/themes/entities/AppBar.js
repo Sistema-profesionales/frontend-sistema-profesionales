6aa6b5b4b81793d7fe64b9a8bb68c3876329402a
camilo.bello92@outlook.es <camilo.bello92@gmail.com>
2020-02-19 20:13:14 -0300

    modify title appbar entities theme

--- src/components/themes/entities/AppBar.js
+++ src/components/themes/entities/AppBar.js
@@ -77,7 +77,7 @@ export default function SearchAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            USUARIO ENTIDAD ♥
+            USUARIO ENTIDAD
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
